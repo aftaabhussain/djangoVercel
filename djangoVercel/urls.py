482b97e9274eebc45189d1e06a7ccdd449ed9661
aftaabhussain <aftaabhussain4505@gmail.com>
2022-12-28 11:57:48 +0530

    vercel json added

--- djangoVercel/urls.py
+++ djangoVercel/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from example import urls as eUrls
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(eUrls)),
+    path('', include('example.urls')),
 ]
